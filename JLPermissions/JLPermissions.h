2321f5fd4eedb2f9d1f2497725c1a1071144fcd7
Georg Kitz <georgkitz@gmail.com>
2014-04-27 17:23:12 +0100

    access to twitter accounts

--- JLPermissions/JLPermissions.h
+++ JLPermissions/JLPermissions.h
@@ -172,6 +172,37 @@ typedef void (^NotificationAuthorizationBlock)(NSString *deviceID,
                   completionHandler:(AuthorizationBlock)completionHandler;
 
 /**
+ *  @return whether or not user has granted access to locations
+ */
+- (BOOL)twitterAuthorized;
+/**
+ *  Uses the default dialog which is identical to the system permission dialog
+ *
+ *  @param completionHandler the block that will be executed on the main thread
+ *when access is granted or denied.  May be called immediately if access was
+ *previously established
+ */
+- (void)authorizeTwitter:(AuthorizationBlock)completionHandler;
+/**
+ *  This is identical to the call other call, however it allows you to specify
+ *your own custom text for the dialog window rather than using the standard
+ *system dialog
+ *
+ *  @param messageTitle      custom alert message title
+ *  @param message           custom alert message
+ *  @param cancelTitle       custom cancel button message
+ *  @param grantTitle        custom grant button message
+ *  @param completionHandler the block that will be executed on the main thread
+ *when access is granted or denied.  May be called immediately if access was
+ *previously established
+ */
+- (void)authorizeTwitterWithTitle:(NSString *)messageTitle
+                            message:(NSString *)message
+                        cancelTitle:(NSString *)cancelTitle
+                         grantTitle:(NSString *)grantTitle
+                  completionHandler:(AuthorizationBlock)completionHandler;
+
+/**
  *  @return whether or not user has granted access to push notifications
  */
 - (BOOL)notificationsAuthorized;
